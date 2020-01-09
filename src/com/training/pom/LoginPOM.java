e371cc3a3aeb894b35e97cee45f4bbebdcf8f7d9
bharatwin7@gmail.com <46638925+bharatwin7@users.noreply.github.com>
2020-01-08 21:55:37 -0500

    first commit

---
+++ src/com/training/pom/LoginPOM.java
@@ -0,0 +1,45 @@
+package com.training.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPOM {
+	private WebDriver driver; 
+	
+	public LoginPOM(WebDriver driver) {
+		this.driver = driver; 
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//a[contains(text(), ' Log In / Register')]")
+	private WebElement loginLink;
+	
+	@FindBy(id="user_login")
+	private WebElement userName; 
+	
+	@FindBy(id="user_pass")
+	private WebElement password;
+	
+	@FindBy(name = "login")
+	private WebElement loginBtn; 
+	
+	public void sendUserName(String userName) throws InterruptedException {
+		this.loginLink.click();
+		Thread.sleep(5000);
+		this.userName.clear();
+		this.userName.sendKeys(userName);
+	}
+	
+	public void sendPassword(String password) {
+		this.password.clear(); 
+		this.password.sendKeys(password); 
+	}
+	
+	public void clickLoginBtn() {
+		
+		
+		this.loginBtn.click(); 
+	}
+}
